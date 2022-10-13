f077a003afedd293b7f8d4390b5f28bb134bfc39
tanakashun <michael021212@gmail.com>
2022-10-13 14:32:52 +0900

    Todo作成

--- main.go
+++ main.go
@@ -11,27 +11,28 @@ func main() {
 	// fmt.Println(config.Config.DbName)
 	// fmt.Println(config.Config.LogFile)
 
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.PassWord = "testtest"
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
+	// u, _ := models.GetUser(1)
+
 	// fmt.Println(u)
 
-	// u.CreateUser()
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u, _ := models.GetUser(1)
-
-	fmt.Println(u)
-
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.DeleteUser()
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
+	// fmt.Println(user)
 }
