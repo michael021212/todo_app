c2e835008c0b1e656e89fc7967548800e5899c29
tanakashun <michael021212@gmail.com>
2022-10-13 17:33:57 +0900

    signup

--- app/contollers/server.go
+++ app/contollers/server.go
@@ -20,6 +20,8 @@ func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...str
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"todo_app/app/contollers"
+	"todo_app/app/models"
 )
 
 func main() {
-	// fmt.Println(models.Db)
+	fmt.Println(models.Db)
 
 	contollers.StartMainServer()
 }
