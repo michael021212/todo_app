8c3adea3e3e8cf61c851a9410de9603153798c6c
tanakashun <michael021212@gmail.com>
2022-10-13 15:01:06 +0900

    サーバー初期設定

---
+++ app/contollers/server.go
@@ -0,0 +1,11 @@
+package contollers
+
+import (
+	"net/http"
+	"todo_app/config"
+)
+
+func StartMainServer() error {
+	http.HandleFunc("/", top)
+	return http.ListenAndServe(":"+config.Config.Port, nil)
+}
